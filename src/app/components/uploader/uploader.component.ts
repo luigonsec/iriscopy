1eb2a5075463feaab1ccedb9fdf5c22f750a0410
Luis González Seco <luisgonzalezseco@gmail.com>
2022-10-21 02:34:21 +0200

    Avances en el formulario

--- src/app/components/uploader/uploader.component.ts
+++ src/app/components/uploader/uploader.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as pdfjsLib from 'pdfjs-dist/legacy/build/pdf.js';
 
 @Component({
   selector: 'app-uploader',
@@ -6,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./uploader.component.scss'],
 })
 export class UploaderComponent implements OnInit {
+  public uploadedFiles: any[] = [];
+  public pages: number = 0;
+  public src: string;
   constructor() {}
 
-  onUpload($event) {}
+  onUpload($event) {
+    const reader = new FileReader();
+    const file = $event.currentFiles[0];
+    this.uploadedFiles = [file];
+    if (file) {
+      reader.readAsBinaryString(file);
+      reader.onloadend = () => {
+        const count = (reader.result as string).match(
+          /\/Type[\s]*\/Page[^s]/g
+        ).length;
 
+        this.pages = count;
+      };
+    }
+  }
   ngOnInit(): void {}
 }
