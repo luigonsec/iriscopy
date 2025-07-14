ee5ca03966569285c7c24c2e9ba44a8085a0d885
Luis González Seco <luis.gonzalez_seco@siemens.com>
2025-07-15 00:47:08 +0200

    fix: correct static file serving path and format code style

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/iriscopy'));
-app.get('/*',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/iriscopy/index.html'));
+app.use(express.static(__dirname + "/dist/iriscopy"));
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname + "/dist/iriscopy/browser/index.html"));
 });
 app.listen(process.env.PORT || 8080);
